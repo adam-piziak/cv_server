8a13f2250754a6a85b7bd935ec52b82cae176863
Adam Piziak <adpiziak@gmail.com>
2020-12-04 20:31:25 -0500

    added botski

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 import random
 import shortuuid
 import math
+import time
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -25,13 +26,14 @@ class Card():
         self.val = card_num % 13
 
 class PlayerState():
-    def __init__(self, key, num, is_host, name):
+    def __init__(self, key, num, is_host, name, is_bot):
         self.key = key
         self.is_host = is_host
         self.card_played = -1
         self.position = num
         self.tricks_won = 0
         self.name = name
+        self.is_bot = is_bot
         self.cards = []
 
     def set_cards(self, cards):
@@ -78,11 +80,11 @@ class GameState():
         self.round_cards = []
         self.last_winner = host
         self.turn_player = 0
-        self.players[host] = PlayerState(host, 0, True, "HOST")
+        self.players[host] = PlayerState(host, 0, True, "HOST", False)
 
     def play_card(self, player_key, card):
         if self.players[player_key].position != self.turn_player:
-            return
+            return False
 
         if not self.round_cards:
             self.round_cards.append(card)
@@ -97,16 +99,56 @@ class GameState():
             self.round_cards.append(card)
             self.players[player_key].play_card(card)
         else:
-            return
+            return False
 
         self.turn_player = (self.turn_player + 1) % 4
 
+
         self.determine_winner()
+        self.check_for_bots()
+        return True
+
+    def check_for_bots(self):
+        turn_player = next(p for p in self.players if self.players[p].position == self.turn_player)
+        turn_player = self.players[turn_player]
+
+        if turn_player.is_bot:
+            if not self.round_cards:
+                #
+                game_state = jsonify(game_state=self.serialize()).get_data(as_text=True)
+                emit("game_state", game_state, room=self.key)
+                time.sleep(1)
+                #
+                self.play_card(turn_player.key, turn_player.cards[0])
+                return
+
+            for i, c in enumerate(turn_player.cards):
+                card = Card(c)
+                #
+                game_state = jsonify(game_state=self.serialize()).get_data(as_text=True)
+                emit("game_state", game_state, room=self.key)
+                time.sleep(.5)
+                #
+                result = self.play_card(turn_player.key, turn_player.cards[i])
+                if result:
+                    return
+
+        else:
+            #game_state = jsonify(self.serialize()).get_data(as_text=True)
+            #emit("game_state", game_state, room=self.key)
+            game_state = jsonify(game_state=self.serialize()).get_data(as_text=True)
+            emit("game_state", game_state, room=self.key)
+            time.sleep(1)
 
     def determine_winner(self):
         if len(self.round_cards) < 4:
             return
 
+        #
+        game_state = jsonify(game_state=self.serialize()).get_data(as_text=True)
+        emit("game_state", game_state, room=self.key)
+        time.sleep(2)
+        #
         winning_idx = 0 # Winning player index
         highest_value = 0
         trump_played = False
@@ -146,10 +188,16 @@ class GameState():
             value.set_cards(deck[i:(i+13)])
             i += 13
 
+    def add_bot(self):
+        new_player = uuid()
+        self.players[new_player] = PlayerState(new_player, len(self.players), False, "BOTSKI " + str(len(self.players)), True)
+        if len(self.players) > 3:
+            self.deal_cards()
+            self.stage = STAGE_PLAYING
 
     def add_player(self, name):
         new_player = uuid()
-        self.players[new_player] = PlayerState(new_player, len(self.players), False, name)
+        self.players[new_player] = PlayerState(new_player, len(self.players), False, name, False)
         if len(self.players) > 3:
             self.deal_cards()
             self.stage = STAGE_PLAYING
@@ -178,6 +226,9 @@ def create_game():
     host = uuid()
     game = GameState(key, host)
     games[key] = game
+    games[key].add_bot()
+    games[key].add_bot()
+    games[key].add_bot()
     return (key, host)
 
 @app.route('/create',methods=['GET', 'POST'])
